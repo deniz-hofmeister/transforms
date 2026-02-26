e2ac8fc9d1b4bb55f8d727f9b44725b6943ad241
carter <carter@amprobotics.com>
2026-02-26 11:19:30 -0700

    Fmt with nightly

--- src/core/registry/mod.rs
+++ src/core/registry/mod.rs
@@ -434,11 +434,11 @@ where
     /// // Get the position of the object (detected at t1) in the world frame at t2
     /// // This answers: "Where is the object now, given it was detected at t1?"
     /// let result = registry.get_transform_at_times(
-    ///     "world",      // target_frame
-    ///     t2,           // target_time (current time)
-    ///     "object",     // source_frame
-    ///     t1,           // source_time (when object was detected)
-    ///     "world",      // fixed_frame
+    ///     "world",  // target_frame
+    ///     t2,       // target_time (current time)
+    ///     "object", // source_frame
+    ///     t1,       // source_time (when object was detected)
+    ///     "world",  // fixed_frame
     /// );
     ///
     /// assert!(result.is_ok());
